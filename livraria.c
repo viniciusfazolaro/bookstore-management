fcfcbc44a4bd7f3de1631f95fa0414baa7aa9032
viniciusfazolaro <140013851+viniciusfazolaro@users.noreply.github.com>
2023-08-20 15:41:38 -0300

    Library exercise

--- livraria.c
+++ livraria.c
@@ -47,6 +47,7 @@ int main(){
             if(consultaCodTitulo == -1){
                 printf("A livraria NÃO possui o livro.\n");
                 getchar();
+                getchar();
             }
             break;
         case 3:
@@ -66,7 +67,7 @@ int main(){
             break;
         default:
             printf("Opção inválida. Tente novamente...");
-            printf("Pressione ENTER para continuar...\n");
+            printf("Pressione ENTER para encerrar...\n");
             getchar();
             getchar();
         }
@@ -90,10 +91,17 @@ void menu(){
 
 Livraria cadastrar(){
     Livraria livraria;
+
+    livraria.anopublicacao = -1, livraria.codigo = -1, livraria.preco = -1;
     system("cls");
     printf("**** Dados do Livro ****\n");
-    printf("Código: ");
-    scanf("%d", &livraria.codigo);
+    do{
+        printf("Código: ");
+        scanf("%d", &livraria.codigo);
+        if(livraria.codigo < 0){
+            printf("Código inválido, o código precisa ser um valor positivo. Tente novamente.\n");
+        }
+    }while(livraria.codigo < 0);
     getchar();
     printf("Título: ");
     gets(livraria.titulo);
@@ -101,10 +109,20 @@ Livraria cadastrar(){
     gets(livraria.primeiroautor);
     printf("Editora: ");
     gets(livraria.editora);
-    printf("Ano de publicação: ");
-    scanf("%d", &livraria.anopublicacao);
-    printf("Preço: ");
-    scanf("%lf", &livraria.preco);
+    do{
+        printf("Ano de publicação: ");
+        scanf("%d", &livraria.anopublicacao);
+        if(livraria.anopublicacao < 0){
+            printf("Ano de publicação inválido, o ano de publicação precisa ser um valor positivo. Tente novamente.\n");
+        }
+    }while(livraria.anopublicacao < 0);
+    do{
+        printf("Preço: ");
+        scanf("%lf", &livraria.preco);
+        if(livraria.preco < 0){
+            printf("Preço inválido, o preço precisa ser um valor positivo. Tente novamente.\n");
+        }
+    }while(livraria.preco < 0);
     printf("******************\n");
     printf("Livro cadastrado com sucesso!\n");
     printf("Pressione ENTER para continuar...\n");
@@ -169,12 +187,12 @@ void consultarPreco(Livraria livros[], int contadorLivros){
     }
 
     if(qtdelivros > 0){
-        printf("Estes são os livros com o preço abaixo de R$%.2lf", pesquisa);
+        printf("Estes são os livros com o preço abaixo de R$%.2lf\n", pesquisa);
         printf("Pressione ENTER para continuar...\n");
         getchar();
         getchar();
     } else{
-        printf("Não foram encontrados livros com o preço abaixo de R$%.2lf", pesquisa);
+        printf("Não foram encontrados livros com o preço abaixo de R$%.2lf\n", pesquisa);
         printf("Pressione ENTER para continuar...\n");
         getchar();
         getchar();
@@ -189,17 +207,23 @@ void consultarAutor(Livraria livros[], int contadorLivros){
     system("cls");
 
     printf("Insira o nome do autor: ");
-    printf("Pressione ENTER para continuar...\n");
     gets(pesquisa);
     gets(pesquisa);
 
+    
     for(i = 0; i < contadorLivros; i++){
         if(strcmp(pesquisa, livros[i].primeiroautor) == 0){
+            printf("%s\n", livros[i].titulo);
             qtdelivros++;
         }
-    }
+    }    
 
-    printf("O autor possui %d livros na livraria.\n", qtdelivros);
+    if(qtdelivros == 0){
+        printf("O autor %s não possui livros na livraria.\n", pesquisa);
+    } else{
+        printf("Estes são os livros do autor %s\n", pesquisa);
+        printf("O autor %s possui %d livros na livraria.\n", pesquisa, qtdelivros);
+    }
     printf("Pressione ENTER para continuar...\n");
     getchar();
     getchar();
@@ -219,8 +243,10 @@ void relatorioLivros(Livraria livros[], int contadorLivros){
         printf("Autor: %s\n", livros[i].primeiroautor);
         printf("Editora: %s\n", livros[i].editora);
         printf("Ano de publicação: %d\n", livros[i].anopublicacao);
-        printf("Pressione ENTER para continuar...\n");
-        getchar();
-        getchar();
+        printf("**********************\n");
     }
+
+    printf("Pressione ENTER para continuar...\n");
+    getchar();
+    getchar();
 }
